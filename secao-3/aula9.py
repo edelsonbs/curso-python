e7f97697ce208a7af71da74480e4866746b6b339
Edelson Barboza <edelsonbsilva@gmail.com>
2023-12-27 15:51:43 -0300

    Operadores aritméticos

---
+++ secao-3/aula9.py
@@ -0,0 +1,25 @@
+adicao = 10 + 10
+print('Adição:', adicao)
+
+subtracao = 10 - 5
+print('Subtração:', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação:', multiplicacao)
+
+divisao = 10 / 2.2  # float
+print('Divisão:', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisão inteira:', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação:', exponenciacao)
+
+modulo = 25 % 5  # resto da divisão
+print('Módulo:', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(16 % 2 == 0)
+print(15 % 2 == 0)
