7d16f8fedf14abe5bdac4be6f190297f9fee67a8
Edelson Barboza <edelsonbsilva@gmail.com>
2023-12-27 12:53:43 -0300

    Exercício com variáveis

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Luiz Otávio'
+sobrenome = 'Miranda'
+idade = 30
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.80
+
+print('Nome:', nome)
+print('Sobronome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
