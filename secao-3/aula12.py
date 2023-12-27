b9920584c7dcf83f4923fbba3b073029fc6e6f6f
Edelson Barboza <edelsonbsilva@gmail.com>
2023-12-27 16:29:26 -0300

    Cálculo IMC

---
+++ secao-3/aula12.py
@@ -0,0 +1,14 @@
+# ... Ellipsis
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+# imc = peso / (altura * altura)
+imc = peso / (altura ** 2)  # IMC = peso / (altura x altura)
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos')
+print('e seu IMC é', imc)
+
+# Luiz Otávio tem 1.8 de altura
+# pesa 95 quilos
+# e seu IMC é 29.320987654320987
