405069408a7760a49f53606ba443e3362f071fe6
Edelson Barboza <edelsonbsilva@gmail.com>
2023-12-28 12:57:19 -0300

    if, elif e else

---
+++ secao-3/aula16.py
@@ -0,0 +1,14 @@
+# if / elif      / else
+# se / se não se / se não
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+  print('Você entrou no sistema')
+
+  print(123412324)
+elif entrada == 'sair':
+  print('Você saiu do sistema')
+else:
+  print('Você não digitou nem entrar nem sair')
+
+print('FORA DOS BLOCOS')
