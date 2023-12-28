6df5c18ae63d2179ee84ad80fe8dded496f181ef
Edelson Barboza <edelsonbsilva@gmail.com>
2023-12-28 15:23:50 -0300

    Operador Lógico "not"

---
+++ secao-3/aula23.py
@@ -0,0 +1,21 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# senha = input('Senha: ')
+
+# if senha == '123456':
+#   print('Entrou')
+# else:
+#   print('Senha incorreta')
+  
+# if senha != '123456':
+#   print('Senha incorreta')
+
+# if not senha:
+#   print('Você não digitou nada')
+
+print(not True)  # False
+print(not 0)  # True
+print(not 1)  # False
