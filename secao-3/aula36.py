e48a54dad432cc41ea4b6b94162f899675b51bff
Edelson Barboza <edelsonbsilva@gmail.com>
2024-01-11 17:44:42 -0300

    Operadores de atribuição com operadores aritméticos

---
+++ secao-3/aula36.py
@@ -0,0 +1,17 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+# contador = 0
+# contador = '1'
+contador = 10
+# contador = 2
+
+###
+
+# contador += 1
+# contador += '2' 
+# contador *= '2' 
+# contador **= 10 
+contador /= 5 
+print(contador)
