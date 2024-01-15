859d4630644a7cb73d74f34f494445ffeddf04d3
Edelson Barboza <edelsonbsilva@gmail.com>
2024-01-15 17:29:52 -0300

    range + for

---
+++ secao-3/aula44.py
@@ -0,0 +1,12 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+# numeros = range(10)
+# numeros = range(5, 10)
+# numeros = range(5, 10, 2)
+# numeros = range(0, -10, -1)
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+  print(numero)
