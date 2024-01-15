7ead8f7530ddc7f8458841a1c1d0f1885062e332
Edelson Barboza <edelsonbsilva@gmail.com>
2024-01-15 16:48:30 -0300

    Iterando strings com while

--- secao-3/aula42.py
+++ secao-3/aula42.py
@@ -13,10 +13,10 @@ while i < len(frase):
     i += 1
     continue
     
-  qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+  qtd_atual = frase.count(letra_atual)
 
-  if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
-    qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+  if qtd_apareceu_mais_vezes <= qtd_atual:
+    qtd_apareceu_mais_vezes = qtd_atual
     letra_apareceu_mais_vezes = letra_atual
     
   i += 1
