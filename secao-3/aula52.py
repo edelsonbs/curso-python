7cd8a45976d46b493dac1ca39f6075b5061c1a46
Edelson Barboza <edelsonbsilva@gmail.com>
2024-01-19 16:38:35 -0300

    Tipo tuple (tupla)

---
+++ secao-3/aula52.py
@@ -0,0 +1,9 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+# nomes = 'Maria', 'Helena', 'Luiz'  # Tupla
+# nomes = ('Maria', 'Helena', 'Luiz')
+nomes = ['Maria', 'Helena', 'Luiz']
+nomes = tuple(nomes)  # coerção -> convertendo uma lista em tuple
+print(nomes[1])
+print(nomes)
