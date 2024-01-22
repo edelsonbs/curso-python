7945163fc80ddc4f348f0ae4497d15522659e0a7
Edelson Barboza <edelsonbsilva@gmail.com>
2024-01-22 16:07:10 -0300

    Operação ternária

---
+++ secao-3/aula60.py
@@ -0,0 +1,14 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+"""
+# condicao = 10 == 11
+# variavel = 'Valor' if condicao else 'Outro valor'
+# print(variavel)
+# digito = 1  # > 9 = 0
+# novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+# print('Valor' if True else 'Outro valor' if True else 'Fim')
+# print('Valor' if False else 'Outro valor' if True else 'Fim')
+print('Valor' if False else 'Outro valor' if False else 'Fim')
