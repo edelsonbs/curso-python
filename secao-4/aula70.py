b967805fb1d92cd51ce55681d22b544673da4ae8
Edelson Barboza <edelsonbsilva@gmail.com>
2024-01-25 14:40:59 -0300

    Retorno de valores das funções

---
+++ secao-4/aula70.py
@@ -0,0 +1,21 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma(x, y):
+  if x > 10:
+    return [10, 20]
+  return x + y
+
+# variavel = print('Luiz')
+# print(variavel)
+# variavel = soma(1, 2)
+# variavel = int('1')
+# print(variavel)
+
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1 + soma2)
+print(soma1)
+print(soma2)
+print(soma(11, 55))
