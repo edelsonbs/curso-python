0da82cc699bb8ea816f265ae37bec5344e6ede8a
Edelson Barboza <edelsonbsilva@gmail.com>
2024-01-26 17:55:58 -0300

    Tipo dict (Dicionários), Manipulando chaves e valores

---
+++ secao-4/aula76.py
@@ -0,0 +1,67 @@
+# Dicionários em Python (tipo dict)
+# Dicionários são estruturas de dados do tipo
+# par de "chave" e "valor".
+# Chaves podem ser consideradas como o "índice"
+# que vimos na lista e podem ser de tipos imutáveis
+# como: str, int, float, bool, tuple, etc.
+# O valor pode ser de qualquer tipo, incluindo outro
+# dicionário.
+# Usamos as chaves - {} - ou a classe dict para criar
+# dicionários.
+# Imutáveis: str, int, float, bool, tuple
+# Mutável: dict, list
+# pessoa = {
+#     'nome': 'Luiz Otávio',
+#     'sobrenome': 'Miranda',
+#     'idade': 18,
+#     'altura': 1.8,
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321},
+#     ]
+# }
+
+# pessoa =  dict(nome='Luiz Otávio', sobrenome='Miranda')
+# pessoa = {
+#     'nome': 'Luiz Otávio',
+#     'sobrenome': 'Miranda',
+#     'idade': 18,
+#     'altura': 1.8,
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321},
+#     ],
+# }
+
+# print(pessoa, type(pessoa))
+# print(pessoa['nome'])
+# print(pessoa['sobrenome'])
+
+# print()
+# for chave in pessoa:
+#   print(chave, pessoa[chave])
+
+# Manipulando chaves e valores em dicionários
+pessoa = {}
+
+#
+chave = 'nome'
+
+pessoa[chave] = 'Luiz Otávio'
+pessoa['sobrenome'] = 'Miranda'
+
+# print(pessoa['nome'])
+print(pessoa[chave])
+pessoa[chave] = 'Maria'
+
+del pessoa['sobrenome']
+print(pessoa)
+print(pessoa['nome'])
+
+# print(pessoa.get('sobrenome'))
+if pessoa.get('sobrenome') is None:
+  print('Existe')
+else:
+  print(pessoa['sobrenome'])
+
+# print('Isso NÃO vai executar')
